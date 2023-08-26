f10be9ef355cb4e3c21394c3965408cec595747c
Sumedh Rasal <sumedh.rasal@here.com>
2023-08-26 18:23:00 -0500

    update config

--- myopenai/openai_config.py
+++ myopenai/openai_config.py
@@ -1,19 +1,24 @@
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain import OpenAI
 from langchain.chat_models import ChatOpenAI
+from dotenv import load_dotenv
 import os
 
 
-os.environ["OPENAI_API_KEY"] = ''
+# Load environment variables from .env file
+load_dotenv()
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
 
 def get_embeddings():
-    return OpenAIEmbeddings(model='text-embedding-ada-002')
+    return OpenAIEmbeddings(model='text-embedding-ada-002', openai_api_key=openai_api_key)
 
 
 def get_model():
-    return OpenAI(model_name='text-ada-001')
+    return OpenAI(model_name='text-ada-001', openai_api_key=openai_api_key)
 
 
 def get_chat_model():
-    return ChatOpenAI()
+    return ChatOpenAI(openai_api_key=openai_api_key)
 
